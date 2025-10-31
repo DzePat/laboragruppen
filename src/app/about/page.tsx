4b16df53cd20ce65c90a33a617e23ade1bb1f1eb
Dzedas Patapovas <126063249+DzePat@users.noreply.github.com>
2025-10-31 13:54:04 +0100

    ADD: -description example in the 'about us' page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,16 +3,9 @@ import Image from "next/image";
 export default function AboutPage() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/logo.png"
-          alt="Next.js logo"
-          width={360}
-          height= {48}
-          priority
-        />
-        <label>About Page</label>
+      <main className="flex flex-col justify-center items-center p-3">
+        <label className="text-xl p-3"> Om oss</label>
+        <p>Laboragruppen AB är en ny aktör inom el- och byggbranschen med specialiserad expertis inom elinstallationer och måleri. Vårt företag erbjuder ett omfattande utbud av installationstjänster och måleri för både privata och kommersiella projekt. Vårt mål är att leverera högkvalitativa lösningar som möter våra kunders unika behov. Laboragruppen AB är även erkänd för vår kompetenta bemanningsverksamhet inom el- och byggsektorn. Vi erbjuder en mångsidig personalpool som inkluderar allt från ambitiösa lärlingar till erfarna yrkesmän och yrkeskvinnor. Vårt mål är att matcha våra kunders projekt med rätt kompetens och erfarenhet för att säkerställa framgångsrika resultat. </p>
         </main>
     </div>
     )
